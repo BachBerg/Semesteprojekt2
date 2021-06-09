be51311beb10d18b64fd85dbfd0dd83f89a7dccf
BachBerg <andreas-bach-nielsen@hotmail.com>
2021-06-09 11:42:31 +0200

    Initial commit

--- src/main/java/org/example/PrimaryController.java
+++ src/main/java/org/example/PrimaryController.java
@@ -9,12 +9,12 @@ public class PrimaryController {
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
 
-        String[] EKGdata = new String[11];
-        Sensor ree= new Sensor();
-        EKGdata = ree.filter(EKGdata);
+        //String[] EKGdata = new String[11];
+        //Sensor ree= new Sensor();
+        //EKGdata = ree.filter(EKGdata);
 
         for (int i = 0; i < 10; i++) {
-            System.out.println(EKGdata[i]);
+            //System.out.println(EKGdata[i]);
         }
 
     }
